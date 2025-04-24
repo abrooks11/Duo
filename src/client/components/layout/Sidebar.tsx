8db2c46a0741e55eb7ee0e6d71470cb837cc0d3e
DESKTOP-MG8LONP\abroo <130313978+abrooks11@users.noreply.github.com>
2025-04-24 13:35:57 -0500

    feat(calendar): add calendar component
    - installed dependencies
    - implemented in SideBar component

--- src/client/components/layout/Sidebar.tsx
+++ src/client/components/layout/Sidebar.tsx
@@ -1,11 +1,13 @@
 // import custom tracker components for Appointments, Claims, and Patients
 import RowFilterList from '../trackers/RowFilterList';
 import ColumnFilterList from '../trackers/ColumnFilterList';
+import FullCalendar from '../trackers/FullCalendar';
 
 function Sidebar() {
   return (
     <div className="sidebar-wrapper">
       {/* <h1>--Sidebar--</h1> */}
+      <FullCalendar />
       <RowFilterList />
       <ColumnFilterList />
     </div>
---
+++ src/client/components/trackers/FullCalendar.tsx
@@ -0,0 +1,21 @@
+import {useState} from 'react'
+import { Calendar } from 'react-date-range';
+
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
+const FullCalendar = () => {
+
+
+
+  return (
+    <div>
+      <h1>Calendar</h1>
+      <Calendar
+        date={new Date()}
+      />
+    </div>
+  );
+};
+
+export default FullCalendar;
